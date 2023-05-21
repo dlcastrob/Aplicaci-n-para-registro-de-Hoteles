ea2056edf1b4d7599460656e1efc0d8c2736a593
dlcastrob <laura.castrobrns@uanl.edu.mx>
2023-05-21 15:00:53 -0600

    lista user

---
+++ Mad/Listas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mad
+{
+    class Listas
+    {
+
+        public class tUser
+        {
+            public string tipoUs { get; set; }
+            public string correoElectronico { get; set; }
+            public string contrasena { get; set; }
+            public string nombreCompleto { get; set; }
+            public string numeroNomina { get; set; }
+            public DateTime fechaNacimiento { get; set; }
+            public string domicilio { get; set; }
+            public string telefonoCasa { get; set; }
+            public string telefonoCelular { get; set; }
+            public string estado { get; set; }
+        }
+
+    }
+}
